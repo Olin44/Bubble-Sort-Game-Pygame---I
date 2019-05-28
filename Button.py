2748a6fa28448f828d7e8033383933ffa9ee4799
Olin44 <47002105+Olin44@users.noreply.github.com>
2019-05-28 20:26:06 +0200

    Add files via upload

--- Button.py
+++ Button.py
@@ -1,24 +1,20 @@
 import pygame
 from game1v2.Resize import Resize
 
-
 class Button(pygame.sprite.Sprite):
-    def __init__(self, key, position, light_postion):
+    def __init__(self, key, position, light_position, scale):
         super().__init__()
         self.key = key
         self.button_position = position
-        self.button_light_position = light_postion
-        self.image_off, self.size_off = Resize(pygame.image.load('./graphics/button_' + self.key + '.png')).resize()
+        self.button_light_position = light_position
+        self.image_off, self.size_off = Resize(pygame.image.load('./graphics/button_' + self.key + '.png'), scale).resize()
         self.size = self.size_off
         self.rect_off = pygame.Rect(self.button_position[0], self.button_position[1], self.size[0], self.size[1])
-        self.image_on, self.size_on = Resize(pygame.image.load('./graphics/button_' + self.key + '_light.png')).resize()
+        self.image_on, self.size_on = Resize(pygame.image.load('./graphics/button_' + self.key + '_light.png'),
+                                             scale).resize()
         self.rect_on = pygame.Rect(self.button_light_position[0], self.button_light_position[1], self.size[0], self.size[1])
         self.rect = self.rect_off
         self.image = self.image_off
-
-    def on_click(self, function):
-        function
-        self.light_off()
 
     def light_on(self):
         self.rect = self.rect_on
--- Frame.py
+++ Frame.py
@@ -2,11 +2,26 @@ import pygame
 from game1v2.Resize import Resize
 
 class Frame(pygame.sprite.Sprite):
-    def __init__(self, x = 269, y = 91):
+    def __init__(self, x, y, scale):
         super().__init__()
         path = './graphics/frame.png'
-        self.image, self.size = Resize(pygame.image.load(path)).resize()
+        self.image, self.size = Resize(pygame.image.load(path), scale).resize()
         self.rect = pygame.Rect(x, y, self.size[0], self.size[1])
+
+    def get_position_x(self):
+        return self.rect[0]
+
+    def get_position_y(self):
+        return self.rect[1]
+
+    def get_coordinate_xy(self):
+        return self.rect[0], self.rect[1]
+
+    def set_position_x(self, x):
+        self.rect[0] = x
+
+    def set_position_y(self, y):
+        self.rect[0] = y
 
     def frame_surface(self):
         return self.image
